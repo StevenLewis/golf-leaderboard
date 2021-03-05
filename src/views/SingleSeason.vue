3e2ef4916f6cad5945d1e91ea74b16f237597610
Steve Lewis <steve@sneekdigital.co.uk>
2021-03-05 11:16:54 +0000

    Removes new competition button when not logged in

--- src/views/SingleSeason.vue
+++ src/views/SingleSeason.vue
@@ -9,7 +9,7 @@
 
         <header class="md:flex justify-between mb-5">
             <h1 class="mb-5 md:mb-0 text-3xl font-bold leading-tight text-gray-900">Season {{ season.name }}</h1>
-            <router-link :to="{ name: 'competitions.create', params: { id: season.id } }" class="hidden flex-none md:flex items-center justify-center px-3 py-2 ml-2 border border-transparent text-sm text-center leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+            <router-link v-if="user.loggedIn" :to="{ name: 'competitions.create', params: { id: season.id } }" class="hidden flex-none md:flex items-center justify-center px-3 py-2 ml-2 border border-transparent text-sm text-center leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
                 New Competition
             </router-link>
         </header>
